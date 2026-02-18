33c8cfa904c1e1b6984a24fc318a60893cc0347e
Nati Zadepschi <zadepschi@yahoo.com>
2026-02-18 15:26:37 +0300

    alias fixed

--- src/widgets/Contacts/ui/Contacts.jsx
+++ src/widgets/Contacts/ui/Contacts.jsx
@@ -7,7 +7,8 @@ import styles from "./Contacts.module.scss";
 import { CONTACT_ITEMS } from "../lib/contacts.config";
 import { Typography } from "@/shared/ui/Typography";
 import contactsData from "@/shared/mockDb/contactsPage.json";
-import { RequestQuoteForm } from "@/features/requestQuote";
+import { RequestQuoteForm } from "@/features/requestQuote/ui/RequestQuoteForm.jsx";
+
 
 
 export const Contacts = () => {
